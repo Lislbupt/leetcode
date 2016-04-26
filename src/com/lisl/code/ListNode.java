bb4a91bf0d0782a1256a9c9bd899a29611640ba8
coolBoy! <853592099@qq.com>
2016-04-26 20:18:10 +0800

    aa

--- src/com/lisl/code/ListNode.java
+++
@@ -1,7 +0,0 @@
-package com.lisl.code;
-
-public class ListNode {
-	int val;
-	ListNode next;
-	ListNode(int x) { val = x; }
-}
--- src/com/lisl/test/Test.java
+++
@@ -1,7 +0,0 @@
-package com.lisl.test;
-
-public class Test {
-	public static void main(String[] args) {
-		
-	}
-}
